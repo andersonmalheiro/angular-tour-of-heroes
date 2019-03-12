e4fdfe2bebdf09d51b415c01fcf63d8b2f01b639
Anderson Malheiro <anderson.malheiro@aluno.fapce.edu.br>
2019-03-12 11:13:57 -0300

    Updating hero detail form with FormBuilder

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -3,6 +3,8 @@ import { Hero } from "../hero";
 
 import { ActivatedRoute, Router } from "@angular/router";
 import { Location } from "@angular/common";
+import { FormBuilder } from "@angular/forms";
+import { Validators } from '@angular/forms';
 
 import { HeroService } from "../hero.service";
 
@@ -12,34 +14,49 @@ import { HeroService } from "../hero.service";
   styleUrls: ["./hero-detail.component.css"]
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero: Hero;
+  hero: Hero;
 
   edit: boolean = true;
+
+  heroForm = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(40)]],
+    role: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(40)]],
+    description: [''],
+    active: ['', Validators.required]
+  });
 
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
     private location: Location,
-    private router: Router
-  ) {}
+    private router: Router,
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit() {
     this.getHero();
+    this.heroForm.disable();
   }
 
   toggleEditMode(): void {
     this.edit = !this.edit;
+    if(this.edit){
+      this.heroForm.disable();
+    } else {
+      this.heroForm.enable();
+    }
   }
 
   getHero(): void {
     const id = this.route.snapshot.paramMap.get("id");
-    this.heroService.getHero(id).subscribe(hero => (this.hero = hero));
-  }
-
-  addHero(hero: Hero): void {
-    this.heroService.addHero(hero).subscribe(
-      _ => this.router.navigate([`/${hero._id}`])
-    )
+    this.heroService.getHero(id).subscribe(hero => (
+      this.hero = hero, 
+      this.heroForm.setValue({
+      name: hero['name'],
+      role: hero.role,
+      description: hero.description,
+      active: hero.active
+    })));
   }
 
   deleteHero(id: string): void {
@@ -49,9 +66,10 @@ export class HeroDetailComponent implements OnInit {
     );
   }
 
-  updateHero(hero: Hero ): void {
-    this.heroService.updateHero(hero).subscribe(
+  updateHero(id: string): void {
+    this.heroService.updateHero(id, this.heroForm.value).subscribe(
       _ => this.toggleEditMode(),
+      hero => this.hero = hero
     );
   }
 
