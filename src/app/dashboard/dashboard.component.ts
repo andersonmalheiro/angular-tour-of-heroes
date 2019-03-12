18740374e3f995a8d385279eaa48d4a9eebad0ea
Anderson Malheiro <anderson.malheiro@aluno.fapce.edu.br>
2019-03-12 11:14:34 -0300

    Showing 3 most liked heroes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -13,7 +13,7 @@ export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
 
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes.slice(1, 5));
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes.slice(0, 3));
   }
 
   constructor(private heroService: HeroService) { }
