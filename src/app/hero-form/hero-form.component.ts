2fb26e3fcd927d4ef29380021845710a55b88c1e
Anderson Malheiro <anderson.malheiro@aluno.fapce.edu.br>
2019-03-12 11:16:39 -0300

    Adding new fields to form with FormBuilder

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
 import { HeroService } from '../hero.service';
+import { Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-hero-form',
@@ -9,13 +11,22 @@ import { HeroService } from '../hero.service';
 })
 export class HeroFormComponent implements OnInit {
 
-  constructor( 
+  heroForm = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(40)]],
+    role: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(40)]],
+    description: [''],
+    active: ['', Validators.required]
+  })
+
+  constructor(
     private heroService: HeroService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder,
   ) { }
 
-  addHero(name: string): void {
-    this.heroService.addHero(name).subscribe(
+  addHero(): void {
+    console.log(this.heroForm.value);
+    this.heroService.addHero(this.heroForm.value).subscribe(
       hero => this.router.navigate([`/detail/${hero._id}`])
     );
   }
